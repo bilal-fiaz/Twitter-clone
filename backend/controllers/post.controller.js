85596e7d315ae6e67e7735c15d082aba728610d3
bilal-fiaz <bilal145j@gmail.com>
2026-04-15 21:43:42 +0500

    Creates getFollowingPosts and getUserPosts routes

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -167,3 +167,50 @@ export const getLikedPosts = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 }
+
+
+export const getFollowingPosts = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        const following = user.following;
+        const feedPosts = await Post.find({ user: { $in: following } }).sort({ createdAt: -1 }).populate({
+            path: "user",
+            select: "-password"
+        }).populate({
+            path: "comments.user",
+            select: "-password"
+        });
+        res.status(200).json(feedPosts);
+
+    } catch (error) {
+        console.log("Error fetching following posts:", error.message);
+        res.status(500).json({ error: error.message });
+    }
+};
+
+
+export const getUserPosts = async (req, res) => {
+    try {
+        const {username} = req.params;
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        const userPosts = await Post.find({ user: user._id }).sort({ createdAt: -1 }).populate({
+            path: "user",
+            select: "-password"
+        }).populate({
+            path: "comments.user",
+            select: "-password"
+        });
+        res.status(200).json(userPosts);
+
+    } catch (error) {
+        console.log("Error fetching user posts:", error.message);
+        res.status(500).json({ error: error.message });
+    }
+}
