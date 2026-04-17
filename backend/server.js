32a220058ab6af2a23f4a8a0dbb634a8127c4776
bilal-fiaz <bilal145j@gmail.com>
2026-04-17 16:29:36 +0500

    Creates Notification routes

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import { v2 as cloudinary } from "cloudinary";
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import postRoutes from "./routes/post.route.js"
+import notificationRoutes from "./routes/notification.route.js"
 
 // Utils imports
 import connectMongoDB from "./db/connectMongoDB.js";
@@ -31,6 +32,7 @@ app.use(cookieParser()); // to parse cookies from incoming requests
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 console.log(process.env.MONGO_URI)
 
