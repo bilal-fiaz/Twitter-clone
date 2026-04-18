7ff54751ffbc8481cb8c7c36b95eea2c19fd4527
bilal-fiaz <bilal145j@gmail.com>
2026-04-18 18:07:10 +0500

    Creates all pages and completed the frondend design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,34 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import HomePage from './pages/home/HomePage'
+import SignUpPage from './pages/auth/signup/SignUpPage'
+import LoginPage from './pages/auth/login/LoginPage'
+import Sidebar from './components/common/SideBar'
+import RightPanel from './components/common/RightPanel'
+import NotificationPage from './pages/notification/NotificationPage.jsx'
+import ProfilePage from './pages/profile/ProfilePage.jsx'
 
 function App() {
-  
 
-  return (
-    <>
-   <button className="btn btn-primary">Primary</button>
-     
-    </>
-  )
+
+	return (
+		<div className='flex max-w-6xl mx-auto'>
+			{/* common component, bc it's not wrapped with routes */}
+			<Sidebar />
+
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='/notifications' element={<NotificationPage />} />
+				<Route path='/profile/:username' element={<ProfilePage />} />
+			</Routes>
+
+
+			<RightPanel />
+		</div>
+
+	)
 }
 
 export default App
